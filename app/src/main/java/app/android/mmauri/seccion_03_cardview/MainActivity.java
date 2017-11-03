72b140c1bfc72073efc2c1d31af84738c0e87bae
Marc from GitKraken <marcmauri92@gmail.com>
2017-11-03 19:56:52 +0100

    Upload project

---
+++ app/src/main/java/app/android/mmauri/seccion_03_cardview/MainActivity.java
@@ -0,0 +1,19 @@
+package app.android.mmauri.seccion_03_cardview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textViewTitle);
+        textView.setText("Hello from the Card View! :)");
+    }
+}
